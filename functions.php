8889216ca48863a5dd67c360c8881c84452f98ed
Pedro Yébenes Segura <pedro.yebenes@uclm.es>
2017-03-22 10:49:18 +0100

    Solved problems with the file names

--- functions.php
+++ functions.php
@@ -1,32 +1,32 @@
 <?php
 function getTemperature(){
-	$command = escapeshellcmd('sudo /home/pi/bin/getTemperature');
+	$command = escapeshellcmd('sudo /home/pi/bin/getTemperature.py');
 	$output = shell_exec($command);
 	return $output;
 }
 function getPressure(){
-	$command = escapeshellcmd('sudo /home/pi/bin/getPressure');
+	$command = escapeshellcmd('sudo /home/pi/bin/getPressure.py');
 	$output = shell_exec($command);
 	return $output;
 }
 function getHumidity(){
-	$command = escapeshellcmd('sudo /home/pi/bin/getHumidity');
+	$command = escapeshellcmd('sudo /home/pi/bin/getHumidity.py');
 	$output = shell_exec($command);
 	return $output;
 }
-
 function showSpainFlag(){
-	$command = escapeshellcmd('sudo /home/pi/bin/spain');
+	$command = escapeshellcmd('sudo /home/pi/bin/spain.py');
 	$output = shell_exec($command);
 	return $output;
 }
 function ledmatrix_clear(){
-	$command = escapeshellcmd('sudo /home/pi/bin/clear');
+	$command = escapeshellcmd('sudo /home/pi/bin/clear.py');
 	$output = shell_exec($command);
 	return $output;
 }
 function showText($text){
-	$command = escapeshellcmd('sudo /home/pi/bin/showtext '.$text);
+	//$command = escapeshellcmd('sudo /home/pi/bin/showtext.py '.$text);
+	$command = 'sudo /home/pi/bin/showtext.py '.$text;
 	$output = shell_exec($command);
 	return $output;
 }
